e9e53e4ee1dc054f2c8e0325afda309799c1579b
zmek <zella@personalboardroom.com>
2020-06-17 17:47:30 +0100

    working-on-ED-edge-list

--- flow-mapping/code/get-ED-data-from-Star.R
+++ flow-mapping/code/get-ED-data-from-Star.R
@@ -6,6 +6,7 @@ library(dplyr)
 library(tidyverse)
 library(lubridate)
 
+# hl7_location seems to have more complete info than room and bed for some rows
 # split location
 split_location <- function(hl7_location, n) {
   s <- unlist(strsplit(hl7_location, "\\^"))
@@ -17,18 +18,21 @@ split_location <- Vectorize(split_location)
 
 # clean room data
 clean_room_names <- function(x) {
-  x = gsub("UCHED ","",x)
-  x = gsub(" BY[0-9]{2}", "", x)
-  x = gsub("BY[0-9]{2} ", "", x)
-  x = gsub(" SR[0-9]{2}", "", x)
-  x = gsub("SR[0-9]{2} ", "", x)
-  x = gsub(" CB[0-9]{2}", "", x)
-  if (length( grep("^T[0-9]{2}",x)) == 0) {
-    x = gsub("[0-9]", "", x)
-  }
-  x = gsub(" CHAIR","",x)
-  x = gsub("MAJ-CHAIR","MAJORS",x)
-  x = gsub("^UTC [A-z]+ ROOM","UTC",x)
+  # x = gsub("UCHED ","",x)
+  # x = gsub(" BY[0-9]{2}", "", x)
+  # x = gsub("BY[0-9]{2} ", "", x)
+  # x = gsub(" SR[0-9]{2}", "", x)
+  # x = gsub("SR[0-9]{2} ", "", x)
+  # x = gsub(" CB[0-9]{2}", "", x)
+  # if (length( grep("^T[0-9]{2}",x)) == 0) {
+  #   x = gsub("[0-9]", "", x)
+  # }
+  x = gsub("CHAIR [0-9]{2}", "CHAIR", x)
+  x = gsub("[0-9]{2}", "BED", x)
+  x = gsub("MAJ-CHAIR","MAJORS CHAIR",x)
+  x = gsub("RAT-CHAIR","RAT CHAIR",x)
+  x = gsub("UCHED RATBED","RAT BED",x)
+  x = gsub("^UTC [A-z]+ ROOM","UTC O/P/T ROOM",x)
   x = gsub(" $","",x)
   return(x)
 }
@@ -90,12 +94,13 @@ bed_moves <- bed_moves %>% mutate(admission_row = ifelse(admission == admission_
 bed_moves <- bed_moves %>% group_by(mrn, csn, encounter_id) %>% 
   mutate(ED_row = ifelse(department == "UCH EMERGENCY DEPT", 1, 0))
 
+ED_bed_moves <- ED_bed_moves %>% mutate(duration_row = difftime(discharge, admission, units = "hours"))
+
 
 # Select only encounters involving ED
 # ===================================
 
 # EITHER generate from bed_moves
-
 # Create summary of all encounters  
 csn_summ <- bed_moves %>% group_by(mrn, csn, encounter_id, admission_dttm, discharge_dttm) %>% 
   summarise(duration = difftime(max(discharge),min(admission), units = "hours"),
@@ -107,86 +112,55 @@ csn_summ <- csn_summ %>% mutate(covid = ifelse(admission_dttm < "2020-03-20 00:0
 # select only encounters with one or more bed_moves involving ED
 ED_csn_summ <- csn_summ %>% filter(nUm_ED_rows > 0) 
 
-# get bed_moves data for those encounters
-ED_bed_moves <- left_join(ED_csn_summ, bed_moves)
-
-
 # save data for future use
 outFile = paste0("EDcrowding/flow-mapping/data-raw/ED_csn_summ_",today(),".rda")
 save(ED_csn_summ, file = outFile)
+
+# OR LOAD saved data
+inFile = paste0("EDcrowding/flow-mapping/data-raw/ED_csn_summ_","2020-06-17",".rda")
+load(inFile)
+
+# Data cleaning for encounters involving ED
+# =========================================
+
+
+# get bed_moves data for those encounters
+ED_bed_moves <- left_join(ED_csn_summ, bed_moves)
+
+# hl7_location seems to have more complete info in some cases where room and bed are NA
+ED_bed_moves <- ED_bed_moves %>% mutate(room2 = case_when(is.na(room) & hl7_location != "ED^null^null" ~ split_location(hl7_location, 2),
+                                                          TRUE ~ room))
+
+# clean room names
+ED_bed_moves <- ED_bed_moves %>% mutate(room3 = clean_room_names(room2))
+# use this to see which room names the function clean_room_names has grouped: 
+e <- ED_bed_moves %>% filter(department == "UCH EMERGENCY DEPT") %>%  group_by(room3, room) %>% summarise(total = n()) 
+
+
+# Replace NAs with None for use in edge list
+ED_bed_moves <- ED_bed_moves %>% mutate(room3 = ifelse(is.na(room3), "None", room3))
+
+# looking at COVID locations - looks like first demaraction of COVID locations happened on 20/3
+ED_bed_moves %>% filter(room2 %in% c( "COVID UTC", "NON COVID UTC", "NON COVID UTC CHAIR")) %>% summarise(min(admission_dttm))
+# ED_bed_moves <- ED_bed_moves %>% mutate(covid = ifelse(admission_dttm < "2020-03-20 12:00:00", "Before", "After"))
+ED_bed_moves %>% group_by(covid) %>% summarise(n())
+
 outFile = paste0("EDcrowding/flow-mapping/data-raw/ED_bed_moves_",today(),".rda")
 save(ED_bed_moves, file = outFile)
 rm(outFile)
 
-# OR load saved data
+# OR load saved ED_bed_moves
 
 inFile = paste0("EDcrowding/flow-mapping/data-raw/ED_bed_moves_","2020-06-15",".rda")
 load(inFile)
 
 
-# Clean room names
-# ================
-
-# Exploring room names
-ED_bed_moves <- ED_bed_moves %>% mutate(room2 = case_when(is.na(room) & hl7_location != "ED^null^null" ~ split_location(hl7_location, 2),
-                                                          TRUE ~ room))
-ED_bed_moves <- ED_bed_moves %>% mutate(room2 = clean_room_names(room2))
 
 
-# use this to see which room names the function clean_room_names has grouped: 
-ED_bed_moves %>% filter(department == "UCH EMERGENCY DEPT") %>%  group_by(room2, room) %>% summarise(total = n()) 
+# CREATE CHARTS
+# =============
 
-
-
-
-ED_bed_moves <- ED_bed_moves %>% mutate(room2 = ifelse(is.na(room2), "None", room2))
-# ED_bed_moves <- ED_bed_moves %>% mutate(duration_row = difftime(discharge, admission, units = "hours"))
-ED_bed_moves %>% filter(department == "UCH EMERGENCY DEPT") %>% group_by(room2) %>% summarise(n())
-
-# looking at COVID locations - looks like reorg for COVID happened on 20/3
-ED_bed_moves %>% filter(room2 %in% c( "COVID UTC", "NON COVID UTC", "NON COVID UTC CHAIR")) %>% summarise(min(admission_dttm))
-# ED_bed_moves <- ED_bed_moves %>% mutate(covid = ifelse(admission_dttm < "2020-03-20 12:00:00", "Before", "After"))
-ED_bed_moves %>% group_by(covid) %>% summarise(n())
-
-
-# load ED pats
-inFile = paste0("data-raw/ED_pats_","2020-06-15",".rda")
-load(inFile)
-
-
-# load flowsheets
-# ===============
-
-
-
-
-# get ED discharge time
-ED_pats 
-ED_discharge_time <- ED_bed_moves %>% filter(ED_row == 1) %>% group_by(encounter_id) %>% summarise(ED_discharge_dttm = max(discharge))
-ED_pats <- ED_pats %>% left_join(ED_discharge_time)
-rm(ED_discharge_time)
-
-
-# get triage end time
-ED_triage_end_time <-  ED_bed_moves %>% filter(ED_row == 1, room2 %in% c("ADULT TRIAGE", "PAEDS TRIAGE")) %>% group_by(encounter_id) %>% summarise(ED_triage_end_time = max(discharge))
-ED_pats <- ED_pats %>% left_join(ED_triage_end_time)
-
-
-# get vital signs for ED patients
-load("~/EDcrowding/flow-mapping/data-raw/flowsheets2020-06-15.rda") #flowsheets
-ED_vital_signs <- ED_pats %>% left_join(flowsheets)
-ED_vital_signs <- ED_vital_signs %>% mutate(taken_in_ED = ifelse(flowsheet_datetime < ED_discharge_dttm, "ED", "Other"))
-ED_vital_signs <- ED_vital_signs %>% mutate(taken_in_triage = ifelse(flowsheet_datetime < ED_triage_end_time, "Triage", "Other"))
-
-vitals_in_ED <- ED_vital_signs %>% filter(!is.na(mapped_name), !is.na(taken_in_ED)) %>%  
-  group_by(mapped_name, taken_in_ED)  %>% summarise(tot = n()) %>%
-  pivot_wider(names_from = taken_in_ED, values_from = tot)
-
-vitals_in_triage <- ED_vital_signs %>% filter(!is.na(mapped_name), !is.na(taken_in_triage)) %>%  
-  group_by(mapped_name, taken_in_triage)  %>% summarise(tot = n()) %>%
-  pivot_wider(names_from = taken_in_triage, values_from = tot)
-
-# Plot of durations in ED
+# Plot of durations in ED - by room
 png("media/Durations in ED.png", width = 1077, height = 659)
 ED_bed_moves %>% filter(department == "UCH EMERGENCY DEPT", as.numeric(duration_row) < 20, as.numeric(duration_row) >= 0, !is.na(covid)) %>%   
   ggplot( aes(x=reorder(room2,as.numeric(duration_row), .fun='median'), y=as.numeric(duration_row), fill=room2)) + 
@@ -197,6 +171,24 @@ ED_bed_moves %>% filter(department == "UCH EMERGENCY DEPT", as.numeric(duration_
        y = "Duration (hours)"
        # fill = "Ventilation support"
        ) +
+  coord_flip() +
+  theme_classic()  + 
+  theme(legend.position="none") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=0)) +
+  facet_wrap(~fct_rev(covid))
+dev.off()
+
+# Plot of durations in ED - by clustered room
+png("media/Durations in ED.png", width = 1077, height = 659)
+ED_bed_moves %>% filter(department == "UCH EMERGENCY DEPT", as.numeric(duration_row) < 20, as.numeric(duration_row) >= 0, !is.na(covid)) %>%   
+  ggplot( aes(x=reorder(room3,as.numeric(duration_row), .fun='median'), y=as.numeric(duration_row), fill=room3)) + 
+  geom_boxplot() +
+  labs(title = "Time spent by patients in ED locations before and after COVID-19 (20.3.20)",
+       subtitle = "Source: Star",
+       x = "",
+       y = "Duration (hours)"
+       # fill = "Ventilation support"
+  ) +
   coord_flip() +
   theme_classic()  + 
   theme(legend.position="none") +
