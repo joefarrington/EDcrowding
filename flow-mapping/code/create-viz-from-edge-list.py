286afd9faeb943b64f7ac1149e521a216543dc10
zmek <zmek@users.noreply.github.com>
2020-07-22 09:03:06 +0100

    Python edits

--- flow-mapping/code/create-viz-from-edge-list.py
+++ flow-mapping/code/create-viz-from-edge-list.py
@@ -5,8 +5,11 @@ import pygraphviz as pgv
 
 # read data
 
-filename = 'flow-mapping/data-output/edgelist_summ-grouped_JanFeb_breachless_2020-07-06.csv'
-df = pd.read_csv(filename, sep = ';')
+filename = '/Users/zellaking/GitHubRepos/EDcrowding/flow-mapping/data-output/edgelist_stats_JanFeb_2020-07-21.csv'
+df = pd.read_csv(filename, sep = ',', dtype = {"weight_mean" : "float64"})
+
+# cheat workaround to get mean to round
+df.weight_mean = df.weight_mean.astype(int)
 
 # initialise graph for example 1
 G = pgv.AGraph(name='Jan-Feb-breachless', directed=True)
@@ -15,20 +18,20 @@ G.node_attr['fixedsize'] = 'false'
 G.node_attr['fontsize'] = '20'
 
 for index, row in df.iterrows():
-    if row['from'] != 'Admitted':
+    if row['from'] != 'Admitted' and row['weight_mean'] > 1:
         G.add_edge(row['from'], row['to'],
-                   weight=row['weight'],
-                   label=row['weight'],
+                   weight=row['weight_mean'],
+                   label=row['weight_mean'],
                    dir="forward",
                    arrowhead="normal",
                    arrowsize=1,
                    style="solid",
                    color='darkseagreen',
      #              penwidth=np.log10(row['weight'],
-                   penwidth=100 * row['weight'] / sum(df['weight'])
+                   penwidth=100 * row['weight_mean'] / sum(df['weight_mean'])
                    )
 
-G.draw("flow-mapping/media/Jan-Feb-breachless.png", prog='dot')
+G.draw("flow-mapping/media/temp.png", prog='dot')
 
 filename = 'flow-mapping/data-output/edgelist_summ_grouped_JanFeb_2020-07-06.csv'
 df = pd.read_csv(filename, sep = ';')
