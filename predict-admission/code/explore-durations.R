ee8a00d2b217cc0406abaadfae6540562d19046b
zmek <zella@personalboardroom.com>
2020-10-26 10:30:13 +0000

    Charts exploring locations 2

--- predict-admission/code/explore-durations.R
+++ predict-admission/code/explore-durations.R
@@ -14,6 +14,13 @@ library(ggalluvial)
 # Create functions (copied from elsewhere)
 # ===============
 
+# Save chart
+save_chart = function(chart_title, g, width = 1077, height = 659) {
+  png(paste0("EDcrowding/predict-admission/media/", chart_title, ".png"), width = width, height = height) 
+  print(g)
+  dev.off()
+}
+
 get_node <- function(dept, room) {
   if (dept == "Still in ED") {
     node <- room
@@ -82,6 +89,16 @@ ED_durations <- ED_bed_moves %>% ungroup() %>%
   mutate(loc_duration = difftime(discharge, admission, units = "mins")) %>% 
   group_by(mrn, csn, epoch, room4) %>% 
   summarise(loc_duration = sum(as.numeric(loc_duration))) 
+
+
+ED_durations_waiting_combined <- ED_bed_moves %>% ungroup() %>% 
+  filter(ED_row_excl_OTF == 1) %>% 
+  select(mrn, csn, room4, epoch, admission, discharge) %>% 
+  mutate(loc_duration = difftime(discharge, admission, units = "mins"),
+         room4 = case_when(room4 %in% c("Arrived", "TRIAGE", "WAITING ROOM", "Waiting") ~ "Waiting",
+                           TRUE ~ room4)) %>% 
+  group_by(mrn, csn, epoch, room4) %>% 
+  summarise(loc_duration = sum(as.numeric(loc_duration)))
 # %>% 
 #   pivot_wider(names_from = room4, names_prefix = "mins_", values_from = loc_duration, values_fill = 0)
 
@@ -93,19 +110,20 @@ ED_durations <- ED_bed_moves %>% ungroup() %>%
 
 
 chart_title = "Frequency of use of ED locations (including outliers)"
-ED_durations %>% ungroup() %>% 
-  left_join(ED_csn_summ %>% select(csn, adm)) %>% 
-  group_by(epoch, adm, room4) %>% 
-  summarise(num_uses = sum(loc_duration>0)) %>% 
-  ggplot(aes(y = num_uses, x = room4, fill = adm)) + geom_bar(stat = "identity") + 
-  theme_classic() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))  +
-  labs(title = chart_title, x = "Location",
-       subtitle = "Arrived = no location information provided in an arrival row\nWaiting = no location information provided in a non-arrival row",
-       fill = "Admitted",
-       y = "Number of encounters") +
-  facet_wrap(epoch~., ncol = 1, scales = "free_y")
-
+save_chart(chart_title, 
+  ED_durations %>% ungroup() %>% 
+    left_join(ED_csn_summ %>% select(csn, adm)) %>% 
+    group_by(epoch, adm, room4) %>% 
+    summarise(num_uses = sum(loc_duration>0)) %>% 
+    ggplot(aes(y = num_uses, x = room4, fill = adm)) + geom_bar(stat = "identity") + 
+    theme_classic() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))  +
+    labs(title = chart_title, x = "Location",
+         subtitle = "Arrived = no location information provided in an arrival row\nWaiting = no location information provided in a non-arrival row",
+         fill = "Admitted",
+         y = "Number of encounters") +
+    facet_wrap(epoch~., ncol = 3, scales = "free_y")
+)
 
 quant_95 <- ED_durations %>% ungroup() %>% 
   group_by(room4) %>% 
@@ -113,39 +131,67 @@ quant_95 <- ED_durations %>% ungroup() %>%
 
 
 chart_title = "Boxplots of duration in ED locations (outliers capped at 95th centile)"
-ED_durations %>%  ungroup() %>% 
-  left_join(ED_csn_summ %>% select(csn, adm)) %>%
-  left_join(quant_95) %>% 
-  mutate(loc_duration = case_when(loc_duration > quant_95 ~ quant_95,
-                                  TRUE ~ loc_duration)) %>% 
-  ggplot(aes(y = loc_duration/60, x = room4, fill = adm, col = adm)) + geom_boxplot(alpha = 0.4) + 
-  theme_classic() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))  +
-  labs(y = "Time in location (hours)", color = NULL, fill = NULL, x = "Admitted",
-       subtitle = "Arrived = no location information provided in an arrival row\nWaiting = no location information provided in a non-arrival row",
-       title = chart_title)+
-  theme(legend.position = "bottom") +
-  facet_wrap(~epoch)
-  
+save_chart(chart_title, 
+             
+  ED_durations %>%  ungroup() %>% 
+    left_join(ED_csn_summ %>% select(csn, adm)) %>%
+    left_join(quant_95) %>% 
+    mutate(loc_duration = case_when(loc_duration > quant_95 ~ quant_95,
+                                    TRUE ~ loc_duration)) %>% 
+    ggplot(aes(y = loc_duration/60, x = room4, fill = adm, col = adm)) + geom_boxplot(alpha = 0.4) + 
+    theme_classic() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))  +
+    labs(y = "Time in location (hours)", color = NULL, fill = NULL, x = "Admitted",
+         subtitle = "Arrived = no location information provided in an arrival row\nWaiting = no location information provided in a non-arrival row",
+         title = chart_title)+
+    theme(legend.position = "bottom") +
+    facet_wrap(~epoch)
+)  
 
 chart_title = "Boxplots of duration in ED locations (excluding TAF, outliers capped at 95th centile)"
-ED_durations %>%  ungroup() %>% 
-  filter(room4 != "TAF") %>% 
-  left_join(ED_csn_summ %>% select(csn, adm)) %>%
-  left_join(quant_95) %>% 
-  mutate(loc_duration = case_when(loc_duration > quant_95 ~ quant_95,
-                                  TRUE ~ loc_duration)) %>% 
-  ggplot(aes(y = loc_duration/60, x = room4, fill = adm, col = adm)) + geom_boxplot(alpha = 0.4) + 
-  theme_classic() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))  +
-  labs(y = "Time in location (hours)", color = NULL, fill = NULL, x = "Admitted",
-       subtitle = "Arrived = no location information provided in an arrival row\nWaiting = no location information provided in a non-arrival row",
-       title = chart_title)+
-  theme(legend.position = "bottom") +
-  facet_wrap(~epoch)
+save_chart(chart_title, 
+           
+           ED_durations %>%  ungroup() %>% 
+              filter(room4 != "TAF") %>% 
+              left_join(ED_csn_summ %>% select(csn, adm)) %>%
+              left_join(quant_95) %>% 
+              mutate(loc_duration = case_when(loc_duration > quant_95 ~ quant_95,
+                                              TRUE ~ loc_duration)) %>% 
+              ggplot(aes(y = loc_duration/60, x = room4, fill = adm, col = adm)) + geom_boxplot(alpha = 0.4) + 
+              theme_classic() +
+              theme(axis.text.x = element_text(angle = 45, hjust = 1))  +
+              labs(y = "Time in location (hours)", color = NULL, fill = NULL, x = "Admitted",
+                   subtitle = "Arrived = no location information provided in an arrival row\nWaiting = no location information provided in a non-arrival row",
+                   title = chart_title)+
+              theme(legend.position = "bottom") +
+              facet_wrap(~epoch)
+
+)
 
 
+quant_95_waiting_combined <- ED_durations_waiting_combined %>% ungroup() %>% 
+  group_by(room4) %>% 
+  summarise(quant_95 = quantile(loc_duration, 0.95, na.rm = TRUE))
 
+chart_title = "Boxplots of duration in ED locations (Waiting areas combined, outliers capped at 95th centile)"
+save_chart(chart_title, 
+           
+           ED_durations_waiting_combined %>%  ungroup() %>% 
+             filter(room4 != "TAF") %>% 
+             left_join(ED_csn_summ %>% select(csn, adm)) %>%
+             left_join(quant_95_waiting_combined) %>% 
+             mutate(loc_duration = case_when(loc_duration > quant_95 ~ quant_95,
+                                             TRUE ~ loc_duration)) %>% 
+             ggplot(aes(y = loc_duration/60, x = room4, fill = adm, col = adm)) + geom_boxplot(alpha = 0.4) + 
+             theme_classic() +
+             theme(axis.text.x = element_text(angle = 45, hjust = 1))  +
+             labs(y = "Time in location (hours)", color = NULL, fill = NULL, x = "Admitted",
+                  subtitle = "Waiting combines Arrived, WAITING ROOM, Waiting and TRIAGE",
+                  title = chart_title)+
+             theme(legend.position = "bottom") +
+             facet_wrap(~epoch)
+           
+)
 # ================
 # Create arc graph
 # ================
